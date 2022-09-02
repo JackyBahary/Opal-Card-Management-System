0d68a10df859d2b5d73ca49f6825ab07f1967c9d
JackyBahary <jackyrichiebahary@gmail.com>
2022-09-02 17:09:36 +1000

    Initial Commit with Navbar using Bootstrap and React.JS

---
+++ opal-card-management-system/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./navigation/navbar.js";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
