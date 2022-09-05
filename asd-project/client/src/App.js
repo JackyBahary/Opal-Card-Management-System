9f5cc41134a1482827dd68492ec722649cfc1309
JackyBahary <jackyrichiebahary@gmail.com>
2022-09-06 01:28:46 +1000

    Update App.js

--- asd-project/client/src/App.js
+++ asd-project/client/src/App.js
@@ -2,15 +2,47 @@ import NavBar from "./components/NavBar";
 import LinkCard from "./pages/LinkCard";
 import LoginRegister from "./pages/LoginRegister";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { useState, useContext, createContext } from "react";
 
 function App() {
+  const [auth, setAuth] = useState<string>();
+
+  async function Login(email, password) {
+    const response = await fetch('http://localhost:8000/api/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const { authenticated } = await response.json();
+    if (authenticated) {
+      setAuth(email);
+    }
+    return authenticated;
+  }
+
+  async function Register(email, password) {
+    const response = await fetch('http://localhost:8000/api/register', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    return data.success;
+  }
+
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path = '/'/>
         <Route path = '/add-card' element = {<LinkCard/>} />
-        <Route path = '/login-register' element = {<LoginRegister/>} />
+        <Route path = '/login-register' element = {
+          <LoginRegister Login={Login} Register={Register} />
+        } />
       </Routes>
     </BrowserRouter>
     
