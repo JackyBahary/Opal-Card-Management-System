2dbf703d03655d432b79178bdfeab84f47f5868a
Josh Celik-Alvis <joshcelik.alvis@gmail.com>
2022-09-06 23:01:02 +1000

    Added Password Change Prompt

--- asd-project/client/src/pages/YourAccount.js
+++ asd-project/client/src/pages/YourAccount.js
@@ -1,21 +1,43 @@
 import { useAuth } from "../App"
+import { useState, useContext, createContext } from "react";
+import Button from "../components/Button"
+
 function YourAccount() {
+  const [password, setPassword] = useState("");
+  const [errorMessage, setSuccessMessage] = useState("");
   const user = useAuth();
+
+  async function HandleSuccess() {
+      setSuccessMessage("Your password has been updated!");
+    }
+
     return (
       
       <div className="container items-center align-center mx-auto w-1/2 bg-gray-900 rounded-xl shadow border p-8 m-10 mt-0">
         <p className="text-4xl text-white font-bold mb-5 text-center pb-8">
           {user}'s Account
         </p>
-          <div className=" justify-self-center w-full grid-cols-2 pb-8">
-            <label className="text-white text-2xl p-8 w-100">New Password </label>
-            <input className="rounded-l text-2xl w-max"></input>
-          </div>
+        
+        {errorMessage && (
+        <div className="rounded-xl justify-end bg-gradient-to-r from-yellow-600 to-red-600 p-2 b-2 text-white font-bold">{errorMessage}</div>
+      )}
+        <div className=" justify-self-center w-full grid-cols-2 pb-8">
+        <label className="text-white text-2xl p-8 w-100">New Password</label>
+        <input className="rounded-l text-2xl w-max"
+          type='password' 
+          onfocus="this.value=''" 
+          value={password} 
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
           <div className="w-full">
-            <button className=" justify-center self-center rounded-xl text-center bg-gradient-to-r from-yellow-600 to-red-600 p-2 px-4 text-white font-extrabold">Change Password</button>
+          
+        <Button
+          type='button' 
+          onChange={e => setPassword(e.target.value)}
+          onClick={HandleSuccess}> Change Password</Button>
           </div>
       </div>
     );
-  }
-  
-  export default YourAccount;
+    }
+    export default YourAccount;
