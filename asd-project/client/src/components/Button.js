8390982313f419c8fbb78916258af00192b0a440
LewisgSmith <77043791+LewisgSmith@users.noreply.github.com>
2022-09-07 09:43:15 +1000

    Fixed button margins in Navbar

--- asd-project/client/src/components/Button.js
+++ asd-project/client/src/components/Button.js
@@ -1,7 +1,7 @@
 function Button(props) {
     const{children, ...others} = props;
     return (
-        <button {...others} className=" rounded-xl justify-end bg-gradient-to-r from-yellow-600 to-red-600 p-2 text-white font-extrabold mx-2 justify-items-center">
+        <button {...others} className=" rounded-xl justify-end bg-gradient-to-r mb-4 from-yellow-600 to-red-600 p-2 text-white font-extrabold mx-2 justify-items-center">
             {children}
         </button>
     );
