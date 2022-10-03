1bc0677d2bd734d3e192437f35cb284e0c9cebfe
JRPK-77 <jefrich5521@gmail.com>
2022-10-03 16:13:41 +0700

    Updated functions for LostStolenCard

--- asd-project/client/src/pages/LostStolenCard.js
+++ asd-project/client/src/pages/LostStolenCard.js
@@ -1,25 +1,55 @@
+import { useState } from "react";
+import Button from "../components/Button"
 
-function LostStolenCard() {
-    return (
+function LostStolenCard({ ReportLostStolenCard }) {
+  const [fullName, setFullName] = useState("");
+  const [opalEmail, setOpalEmail] = useState("");  
+  const [opalCardNumber, setOpalCardNumber] = useState("");  
+  const [errorMessage, setErrorMessage] = useState("");  
+  
+  async function HandleLostStolenCard() {
+    const success = await ReportLostStolenCard(fullName, opalEmail, opalCardNumber);
+    if (!success) {
+      setErrorMessage("Invalid Input");
+    }
+    else {
+      alert("Successfully reported Lost/Stolen Card");
+    }
+  }
+
+  return (
       <div className="container items-center align-center mx-auto w-1/2 bg-gray-900 rounded-xl shadow border p-8 m-10 mt-0">
         <p className="text-4xl text-white font-bold mb-5 text-center pb-8">
           Request Lost/Stolen Card
         </p>
+        {errorMessage && (
+        <div className="rounded-xl justify-end bg-gradient-to-r from-yellow-600 to-red-600 p-2 b-2 text-white font-bold">{errorMessage}</div>
+      )}
           <div className=" justify-self-center w-full grid-cols-2 pb-8">
             <label className="text-white text-2xl p-8 w-100">Full Name</label>
-            <input className="rounded-l text-2xl w-max"></input>
+            <input className="rounded-l text-2xl w-max" 
+            required
+            value={fullName} 
+          onChange={e => setFullName(e.target.value)}> </input>
           </div>
           <div className=" justify-self-center w-full grid-cols-2 pb-8">
             <label className="text-white text-2xl p-8 w-100">Email</label>
-            <input className="rounded-l text-2xl w-max"></input>
+            <input className="rounded-l text-2xl w-max" 
+            required
+            value={opalEmail} 
+          onChange={e => setOpalEmail(e.target.value)}> </input>
           </div>
           <div className=" justify-self-center w-full grid-cols-2 pb-8">
             <label className="text-white text-2xl p-8 w-100">Opal Card Number</label>
-            <input className="rounded-l text-2xl w-max"></input>
+            <input className="rounded-l text-2xl w-max" 
+            required
+            maxLength={20}
+            value={opalCardNumber} 
+          onChange={e => setOpalCardNumber(e.target.value)}> </input>
           </div>
           <div className="w-full">
-            <button className=" justify-center self-center rounded-xl text-center bg-gradient-to-r from-yellow-600 to-red-600 p-2 px-4 text-white font-extrabold">Submit</button>
-          </div>
+            <Button type='button' onClick={HandleLostStolenCard}>Submit</Button>
+          </div>  
       </div>
     );
   }
