6177cefc6ddac00d7208840c2bbf142459b26ebf
JackyBahary <jackyrichiebahary@gmail.com>
2022-10-04 13:19:44 +1100

    Testing

---
+++ asd-project/client/cypress/integration/RecordTripTest.js
@@ -0,0 +1,18 @@
+// RecordTripTest.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe('Visiting Record Trips Page', () => {
+    it('visits', () => {
+      cy.visit('http://localhost:3000/')
+      cy.get('input:first').type('jacky@gmail.com')
+      cy.get('input').eq(1).type('jacky123')
+      cy.get('button:first').click()
+      cy.contains('Trip History').click()
+      cy.get('select').select(1)
+      cy.contains('View History').click()
+      cy.get('tbody').find('tr').should('have.length', 3)
+    })
+  })
