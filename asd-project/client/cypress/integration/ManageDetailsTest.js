55c03893eb599d34e74e5013091d3c28de8045b9
JackyBahary <jackyrichiebahary@gmail.com>
2022-10-07 00:08:55 +1100

    Josh Report Section

--- asd-project/client/cypress/integration/ManageDetailsTest.js
+++ asd-project/client/cypress/integration/ManageDetailsTest.js
@@ -1,18 +1,19 @@
-// TripHistoryTest.js created with Cypress
+// ManageDetailsTest.js created with Cypress
 //
 // Start writing your Cypress tests below!
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-describe('Visiting Trip History Page', () => {
+describe('Visiting Your Account Page', () => {
     it('visits', () => {
       cy.visit('http://localhost:3000/')
       cy.get('input:first').type('jacky@gmail.com')
       cy.get('input').eq(1).type('jacky123')
       cy.get('button:first').click()
-      cy.contains('Trip History').click()
-      cy.get('select').select('2173964')
-      cy.contains('View History').click()
-      cy.get('tbody').find('tr').its('length').should('be.gt', 0)
+      cy.contains('Your Account').click()
+      cy.get('input').type('test')
+      cy.get('button').contains(/^Change Your Password$/).click()
+      cy.wait(5000)
+      cy.contains('Successfully Changed Password')  
     })
   })
