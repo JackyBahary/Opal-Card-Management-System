5c79f88088daa35596de0d40f18dbfb70b3c0f00
Josh Celik-Alvis <joshcelik.alvis@gmail.com>
2022-10-23 13:25:33 +1100

    Top Up UI Changes

--- asd-project/client/src/pages/TopUp.js
+++ asd-project/client/src/pages/TopUp.js
@@ -1,14 +1,27 @@
 import { useAuth } from "../App"
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button"
 
-function TopUp({UpdateBalance}) {
+function TopUp({Cards, UpdateBalance}) {
     const navigate = useNavigate();
     const [amount, setAmount] = useState("");
+    const [cards, setCards] = useState([]);
+    const [card, setCard] = useState();
     const [errorMessage, setSuccessMessage] = useState("");
+    const [clicked, setClicked] = useState();
     const user = useAuth();
 
+  //Handle function to get cards from DB
+  async function HandleCards() {
+    const cards = await Cards(user);
+    setCards(cards);
+    setCard(cards[0].cardnumber);
+  }
+  //This is to call functions as page loads
+  useEffect(() => {
+    HandleCards()
+  }, [])
 
     //updates users balance to old value + new value
     async function HandleTopUp() {
@@ -41,14 +54,31 @@ function TopUp({UpdateBalance}) {
           />
         </div>
 
-        <div className="w-full">    
-        <Button
-          type='button' 
-          onChange={e => setAmount(e.target.value)}
-          //calls HandleTopUp on button select
-          onClick={HandleTopUp}> Top Up</Button>
+        <div className="container items-center align-center mx-auto w-1/2 bg-gray-900 rounded-xl shadow border p-8 m-10 mt-0">
+        <p className="text-4xl text-white font-bold mb-5 text-center pb-8">
+            Your Cards
+        </p>
+        <div className=" justify-self-center w-full grid-cols-2 pb-8 mt-6">
+            <label className="text-white text-2xl p-8 w-100">Cards</label>
+            {/* map select tag with every index in 'cards' state */}
+            <select className="rounded-l text-2xl w-max" 
+            value={card} 
+            onChange={e => {setCard(parseInt(e.target.value))
+            setClicked(false)}}
+            >
+              {
+                cards.map((card) => {
+                  return (
+                    <option key = {card.cardnumber} value={card.cardnumber}>{card.cardnumber}</option>
+                  )
+                })
+              }
+            </select>
+            <Button type='button'
+            onClick={HandleTopUp}>Top Up</Button> {/*Call handle function when button is clicked*/}
         </div>
-      </div>
+        </div>
+        </div>
     );
     }
     export default TopUp;
